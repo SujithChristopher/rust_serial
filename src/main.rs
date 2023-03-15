5324b71b0c60c349938cd98aa6361e8aeecd819a
Sujith Christopher <chrisbon95@gmail.com>
2023-03-15 16:47:31 +0530

    able to detect 255 255

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 use csv::Writer;
 use serialport::SerialPortBuilder;
 
+
 mod csv_support;
 
 fn main() {
@@ -44,18 +45,89 @@ fn main() {
         .unwrap();
 
     let mut buf: [u8; 1] = [0; 1];
+
+    let mut header1 = 0;
+    let mut header2 = 0;
+    let mut databytes = 0;
     loop {
-        match port.read(&mut buf) {
+        // read from serial port
+        header1 = match port.read(&mut buf) {
             Ok(t) => {
                 if t > 0 {
-                    println!("Read: {}", buf[0]);
+                    buf[0]
+                } else {
+                    0
                 }
             }
-            Err(e) => println!("Error: {}", e),
+            Err(e) => {
+                println!("Error: {}", e);
+                0
+            }
+        };
+        // read from serial port
+        header2 = match port.read(&mut buf) {
+            Ok(t) => {
+                if t > 0 {
+                    buf[0]
+                } else {
+                    0
+                }
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                0
+            }
+        };
+
+        if header1 == 255 && header2 == 255 {
+            // read from serial port
+            databytes = match port.read(&mut buf) {
+                Ok(t) => {
+                    if t > 0 {
+                        buf[0]
+                    } else {
+                        0
+                    }
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                    0
+                }
+            };
+
+            // define a vector to hold the values size of val3
+            let mut vec = Vec::with_capacity(databytes as usize);
+            
+            for i in 0..databytes {
+                // read from serial port
+                let val = match port.read(&mut buf) {
+                    Ok(t) => {
+                        if t > 0 {
+                            buf[0]
+                        } else {
+                            0
+                        }
+                    }
+                    Err(e) => {
+                        println!("Error: {}", e);
+                        0
+                    }
+                };
+
+                vec.push(val);
+            }
+            // print the vector 0 to 3
+            // println!("Read: {:?}", vec);
+
+            // convert first 4 bytes to float
+            let mut bytes = [0u8; 4];
+            bytes[0] = vec[0];
+            bytes[1] = vec[1];
+            bytes[2] = vec[2];
+            bytes[3] = vec[3];
+            let f = f32::from_le_bytes(bytes);
+            println!("Float: {}", f);
         }
+
     }
-
-
 }
-
-
